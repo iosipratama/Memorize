e095c666dfc236da4a9b538f6aa65f7334333ce9
Iosi Pratama <pratamaiosi@gmail.com>
2024-01-05 04:54:34 +0700

    Add initial layout similar to the lecture

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -9,15 +9,46 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("My First commit")
+        HStack {
+            CardView()
+            CardView()
+            CardView(isOpen: true)
+            CardView()
+            
         }
         .padding()
     }
 }
+
+struct CardView: View {
+    
+    var isOpen: Bool = false
+    
+    var body: some View {
+        
+        if isOpen == false {
+            ZStack {
+                RoundedRectangle(cornerRadius: 20)
+                    .foregroundStyle(.white)
+                    .overlay (
+                        RoundedRectangle(cornerRadius: 20)
+                            .stroke(.black, lineWidth: 2)
+                    )
+                
+                Text("😭")
+            }
+        } else {
+            
+            ZStack {
+                RoundedRectangle(cornerRadius: 20)
+                    .foregroundStyle(.black)
+            }
+            
+        }
+    }
+}
+
+
 
 #Preview {
     ContentView()
