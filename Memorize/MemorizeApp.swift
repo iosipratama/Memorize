e68844a327de53ab82f028a1b8e9b7caef40debe
Iosi Pratama <pratamaiosi@gmail.com>
2024-01-04 16:03:06 +0700

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Iosi Pratama on 04/01/24.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
